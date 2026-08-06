574b04ff5ab7e147faa098f1fd9cc140c5a6e1cc
ifsbibi <evilyngabrielly05@gmail.com>
2026-08-06 14:52:47 -0300

    aaaaaaaaaaa

--- src/main/java/com/evilyn/estoque/controller/CadastroController.java
+++ src/main/java/com/evilyn/estoque/controller/CadastroController.java
@@ -1,11 +1,18 @@
 package com.evilyn.estoque.controller;
 
+import com.evilyn.estoque.model.Usuario;
 import com.evilyn.estoque.model.UsuarioDAO;
+import com.evilyn.estoque.util.GerenciadorTela;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.TextFlow;
 
-import java.awt.*;
+import java.io.IOException;
+
 
 public class CadastroController {
 
@@ -19,20 +26,45 @@ public class CadastroController {
     private PasswordField confirmaSenha;
 
     @FXML
+    private Label usuarioInvalido;
+
+    @FXML
+    private  Label senhaInvalida;
+
+    @FXML
     private TextFlow erroSenha;
 
     private static UsuarioDAO dbUsuario = UsuarioDAO.getInstancia();
 
     @FXML
-    protected void aoConfirmarCadastro(){
-        String
+    protected  void aoConfirmarCadastro(ActionEvent event) throws IOException {
+        String usuario =  usuarioCadastrar.getText();
+        if(usuario.isBlank()){
+            usuarioInvalido.setVisible(true);
+            return;
+        }
 
+        String senha =  senhaCadastrar.getText();
+        if(senha.isBlank()){
+            senhaInvalida.setVisible(true);
+            return;
+        }
+
+        String senhaConfirmacao = confirmaSenha.getText();
+        if(!senhaConfirmacao.equals(senha)){
+            erroSenha.setVisible(true);
+            return;
+        }
+
+        Usuario novoUsuario =  new Usuario(usuario, senha);
+        dbUsuario.cadastrarUsuario(novoUsuario);
+
+        GerenciadorTela.getInstancia().trocarTela(event, "login.fxml", "Sistema de Estoque - Login");
     }
 
     @FXML
-    protected void aoAcessarLogin(){
+    protected void  aoAcessarLogin(MouseEvent event) throws IOException {
+        GerenciadorTela.getInstancia().trocarTela(event, "login.fxml", "Sistema de Estoque - Login");
 
     }
-
-
 }
